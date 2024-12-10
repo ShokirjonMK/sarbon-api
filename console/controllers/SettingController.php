750e10c0e980cf67c7cb889d7ded20a18173446b
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2024-12-10 14:22:17 +0500

    Update SettingController.php

--- console/controllers/SettingController.php
+++ console/controllers/SettingController.php
@@ -4,6 +4,9 @@ namespace console\controllers;
 
 use common\models\model\Group;
 
+use common\models\model\Profile;
+use common\models\model\Student;
+use common\models\User;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 use yii\console\Controller;
@@ -26,35 +29,35 @@ class SettingController extends Controller
                 $role = 'student';
                 if (isset($group)) {
 
-//                    $model = new User();
-//                    $user = self::studentLogin();
-//                    $model->username = $user['username'];
-//                    $model->email= $user['email'];
-//                    $password = _passwordMK();
-//                    $model->password_hash = \Yii::$app->security->generatePasswordHash($password);
-//                    $model->auth_key = \Yii::$app->security->generateRandomString(20);
-//                    $model->password_reset_token = null;
-//                    $model->access_token = \Yii::$app->security->generateRandomString();
-//                    $model->access_token_time = time();
-//                    $model->status = 10;
-//                    $model->save(false);
+                    $model = new \common\models\model\User();
+                    $user = self::studentLogin();
+                    $model->username = $user['username'];
+                    $model->email= $user['email'];
+                    $password = _passwordMK();
+                    $model->password_hash = \Yii::$app->security->generatePasswordHash($password);
+                    $model->auth_key = \Yii::$app->security->generateRandomString(20);
+                    $model->password_reset_token = null;
+                    $model->access_token = \Yii::$app->security->generateRandomString();
+                    $model->access_token_time = time();
+                    $model->status = 10;
+                    $model->save(false);
 //
-//                    $model->savePassword($password, $model->id);
-//
-//                    $profile = new Profile();
-//                    $profile->user_id = $model->id;
-//                    $profile->save(false);
-//
-//                    $student = new Student();
-//                    $student->group_id = $group->id;
-//                    $student->user_id = $model->id;
-//                    $student->type = 1;
-//                    $student->status = 10;
-//                    $student->save(false);
-//
-//                    $auth = \Yii::$app->authManager;
-//                    $authorRole = $auth->getRole($role);
-//                    $auth->assign($authorRole, $model->id);
+                    $model->savePassword($password, $model->id);
+
+                    $profile = new Profile();
+                    $profile->user_id = $model->id;
+                    $profile->save(false);
+
+                    $student = new Student();
+                    $student->group_id = $group->id;
+                    $student->user_id = $model->id;
+                    $student->type = 1;
+                    $student->status = 10;
+                    $student->save(false);
+
+                    $auth = \Yii::$app->authManager;
+                    $authorRole = $auth->getRole($role);
+                    $auth->assign($authorRole, $model->id);
 
                 } else {
                     $errors[] = $dataOne[1];
@@ -81,7 +84,7 @@ class SettingController extends Controller
 
         $result['username'] = 'sarbon-std-' . $count;
         if (!(isset($post['email']))) {
-            $result['email'] = 'sarbon-std' . $count . '@sarbon.uz';
+            $result['email'] = 'sarbon-std-' . $count . '@sarbon.uz';
         }
         return $result;
     }
