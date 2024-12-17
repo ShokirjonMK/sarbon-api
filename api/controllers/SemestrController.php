d5a547f07f9a816715dac98101eceda7bc0f8d9d
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2024-12-17 15:46:10 +0500

    Update SemestrController.php

--- api/controllers/SemestrController.php
+++ api/controllers/SemestrController.php
@@ -52,7 +52,7 @@ class SemestrController extends ApiActiveController
 
     public function actionCreate($lang)
     {
-        return $this->response(0, _e('There is an error occurred while processing.'), null, null, ResponseStatus::FORBIDDEN);
+//        return $this->response(0, _e('There is an error occurred while processing.'), null, null, ResponseStatus::FORBIDDEN);
         $model = new Semestr();
         $post = Yii::$app->request->post();
         $this->load($model, $post);
