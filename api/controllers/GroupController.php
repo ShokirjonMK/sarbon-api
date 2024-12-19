199e2d523ff1067d350402daa27f1fcd5623b12b
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2024-12-19 17:58:38 +0500

    Update GroupController.php

--- api/controllers/GroupController.php
+++ api/controllers/GroupController.php
@@ -38,7 +38,7 @@ class GroupController extends ApiController
         $model = new Group();
 
         $query = $model->find()
-            ->andWhere(['is_deleted' => 0])
+//            ->andWhere(['is_deleted' => 0])
 //            ->with(['infoRelation'])
 //            ->andWhere([$this->table_name . '.is_deleted' => 0])
 //            ->leftJoin("translate tr", "tr.model_id = $this->table_name.id and tr.table_name = '$this->table_name'")
