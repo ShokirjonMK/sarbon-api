a1a523010c1767d683780967c70552f04f9d4658
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-01-04 16:36:57 +0500

    test topic_id null alter

---
+++ console/migrations/m250104_113545_change_topic_id_nullable.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m250104_113545_change_topic_id_nullable
+ */
+class m250104_113545_change_topic_id_nullable extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->alterColumn('{{%test}}', 'topic_id', $this->integer()->null());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m250104_113545_change_topic_id_nullable cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m250104_113545_change_topic_id_nullable cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
